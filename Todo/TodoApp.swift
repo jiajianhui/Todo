b1c9d6230666281bb822477cb902da1680fd5835
jianhui <jia15176168273@icloud.com>
2023-12-16 16:14:15 +0800

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by 贾建辉 on 2023/12/16.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
