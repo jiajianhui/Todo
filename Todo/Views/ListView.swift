f170fe477101fe26618f8f3f3fbcdd1c24ec181c
jianhui <jia15176168273@icloud.com>
2023-12-26 22:38:39 +0800

    Add SwipeActions Packages

--- Todo/Views/ListView.swift
+++ Todo/Views/ListView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwipeActions
 
 struct ListView: View {
     
@@ -25,14 +26,42 @@ struct ListView: View {
         NavigationView {
             ScrollView {
                 if !filterLists.isEmpty {
-                    LazyVStack(spacing: 12) {
-                        ForEach(filterLists.indices, id: \.self) { index in  //获取集合索引的方式，它返回一个范围
-                            
-                            NavigationLink {
-                                ListDetailView(listItem: $filterLists[index])
+                    SwipeViewGroup {
+                        LazyVStack(spacing: 12) {
+                            ForEach(filterLists.indices, id: \.self) { index in  //获取集合索引的方式，它返回一个范围
                                 
-                            } label: {
-                                ListRowView(listItem: filterLists[index])
+                                NavigationLink {
+                                    ListDetailView(listItem: $filterLists[index])
+                                    
+                                } label: {
+                                    SwipeView {
+                                        ListRowView(listItem: filterLists[index])
+                                    } trailingActions: { context in
+
+                                        SwipeAction(
+                                            filterLists[index].collected ? "取消收藏" : "收藏",
+                                            backgroundColor: .orange.opacity(0.05)
+                                        ) {
+                                            context.state.wrappedValue = .closed
+                                            collect(at: index)
+                                        }
+                                        .font(.title3.weight(.bold))
+                                        .foregroundColor(.orange)
+
+                                        SwipeAction(
+                                            "删除",
+                                            backgroundColor: .red.opacity(0.05)
+                                        ) {
+                                            context.state.wrappedValue = .closed
+                                            delete(at: index)
+                                        }
+                                        .font(.title3.weight(.bold))
+                                        .foregroundColor(.red)
+                                    }
+                                    .swipeActionsMaskCornerRadius(12) //控制整个滑动按钮区域的圆角
+                                    .swipeActionCornerRadius(12)  //控制内部单个按钮的圆角
+                
+                                    
                                     .swipeActions(allowsFullSwipe: true) {
                                         Button("删除", role: .destructive) {  //使用role，删除时会有过渡效果
                                             delete(at: index)
@@ -43,20 +72,20 @@ struct ListView: View {
                                         }
                                         .tint(.orange)
                                     }
-                                    
+                                        
+                                }
                             }
-                            
-                            .listRowSeparator(.hidden)  //隐藏分割线
                         }
-                    }
-                    .foregroundColor(.primary)
+                        .foregroundColor(.primary)
                     .padding(.horizontal, 16)
+                    }
                 } else if pickerValue == "收藏" && filterLists.filter{$0.collected}.isEmpty {
                     NoListView(title: "暂无收藏内容", image: "star")
                 } else {
                     NoListView(title: "赶快添加你的第一个想法", image: "bubbles.and.sparkles")
                 }
             }
+            
             .navigationTitle("灵感列表")
             .toolbar {
                 ToolbarItem(placement: .principal) {
