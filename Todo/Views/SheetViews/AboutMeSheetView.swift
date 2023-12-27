c4b2d199b66d93746f2d1fe58da8cff55cacfa68
jianhui <jia15176168273@icloud.com>
2023-12-27 22:50:38 +0800

    Change SheetViews Content

--- Todo/Views/SheetViews/AboutMeSheetView.swift
+++ Todo/Views/SheetViews/AboutMeSheetView.swift
@@ -19,8 +19,8 @@ struct AboutMeSheetView: View {
                 VStack(alignment: .leading, spacing: 16) {
                     Text("👋")
                         .font(.system(size: 46))
-                    Text("创造深植于内心，上架一款APP是我一直以来的心愿")
-                    Text("进入新的领域，学习新的知识，从来都是让我无比兴奋的事情；这一次我使用 SwiftUI 来完成这一款APP，过程充满乐趣与挑战，在经历过无数次 Build Succeeded 后，终于完成了这款APP")
+                    Text("创造深植于内心，上架一款APP是我一直以来的心愿。")
+                    Text("进入新的领域，学习新的知识，从来都是让我无比兴奋的事情；这一次我使用 SwiftUI 来完成这一款APP，过程充满乐趣与挑战，在经历过无数次 Build Succeeded 后，终于完成了这款APP。")
                 }
                 .lineSpacing(7)
                 .padding(.horizontal, 16)
--- Todo/Views/SheetViews/DesignSheetView.swift
+++ Todo/Views/SheetViews/DesignSheetView.swift
@@ -17,8 +17,8 @@ struct DesignSheetView: View {
                 VStack(alignment: .leading, spacing: 16) {
                     Text("🫧")
                         .font(.system(size: 46))
-                    Text("头脑中的灵感转瞬即逝，就像泡泡；快速捕捉这些灵感是这款APP的核心功能")
-                    Text("大多数类似的产品都能满足上述需求，但是又不够专一和纯粹，夹杂着太多功能，让人望而却步")
+                    Text("头脑中的灵感转瞬即逝，就像泡泡。")
+                    Text("我想做一款简单的App，记录这些灵感，让灵感持续发生。")
                 }
                 .lineSpacing(7)
                 .padding(.horizontal, 16)
